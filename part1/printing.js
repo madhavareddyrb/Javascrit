626d1ec524103c8895ba58acb3f872af6553545e
madhavareddyrb <madhavareddyrb@gmail.com>
2025-08-19 19:30:20 +0530

    day1

---
+++ part1/printing.js
@@ -0,0 +1,8 @@
+console.log("Hello Js")
+console.log("hello")
+
+process.stdout.write('stdout method') 
+process.stdout.write('stdout method')
+
+console.table({name1:'madhava'})
+console.warn({city: "Hyderbad"})
