797d61f3e870f12aa9e79fe806786d88167aced1
madhavareddyrb <madhavareddyrb@gmail.com>
2025-09-11 08:12:34 +0530

    Iterators and Generators

---
+++ part9_adv/iteratorsGeneratoes.js
@@ -0,0 +1,20 @@
+function* number(){
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+let gen = number();
+let genTwo = number();
+
+console.log(gen);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+
+console.log(genTwo.next().value);
+console.log(genTwo.next().value);
+
+
+
